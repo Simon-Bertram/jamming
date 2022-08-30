88b6724b72ca2fefdac64429e2d842f905d66c07
Simon-Bertram <50182890+Simon-Bertram@users.noreply.github.com>
2022-08-30 15:14:51 +0100

    initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Ja<span class="highlight">mm</span>ing
+      </h1>
+      <div class="App">
+        <!-- Add a SearchBar component -->
+        <div class="App-playlist">
+          <!-- Add a SearchResults component -->
+          <!-- Add a Playlist component -->
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
