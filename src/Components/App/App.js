793eb6e6bbb1be4519d1e72936e054d0d389607e
Simon-Bertram <50182890+Simon-Bertram@users.noreply.github.com>
2022-09-01 15:46:37 +0100

    Add state to App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,36 @@
+import React from "react";
 import "./App";
 import "./App.css";
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import PlayList from "../Playlist/Playlist";
 
-function App() {
-  return (
-    <div>
-      <h1>
-        Ja<span class="highlight">mm</span>ing
-      </h1>
-      <h2>Heading 2</h2>
-      <div class="App">
-        <SearchBar />
-        <div class="App-playlist">
-          <SearchResults />
-          <PlayList />
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [{songTitle: 'Tiny Dancer', artist: 'Elton John', album: 'Madman', id: 1},
+        {songTitle: 'Lion King', artist: 'Elton Johns Brother', album: 'Madman2', id: 2}
+      ]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span class="highlight">mm</span>ing
+        </h1>
+        <h2>Heading 2</h2>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <PlayList />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
